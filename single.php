f0db466abf12a0c01e4a8411acbedaf8c5364ed0
nagai113 <nagai@mousousakusen.com>
2013-02-11 18:01:32 +0900

    投稿情報の変更

--- single.php
+++ single.php
@@ -7,10 +7,12 @@
               <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
                 <div class="article" id="post-<?php the_ID(); ?>">
                       <h1><?php the_title(); ?></h1>
+                      <br>
                       <div class="postmetadata">
-                          Posted: <?php the_time(__('F jS, Y')) ?>&nbsp;&#721;&nbsp;<?php printf(__('Filled under: %s'), get_the_category_list(', ')); ?>&nbsp;&#721;&nbsp;
+                          <?php the_time(__('F jS, Y')) ?>&nbsp;&#721;&nbsp;<?php printf(__('カテゴリー: %s'), get_the_category_list(', ')); ?>&nbsp;&#721;&nbsp;
                           <!--<?php comments_popup_link(__('No Comments'), __('1 Comment'), __('% Comments'), '', __('Comments Closed') ); ?><?php edit_post_link(__('Edit this entry'), '&nbsp;&#721;&nbsp;', ''); ?>-->
                       </div>
+                      <br>
                       <?php the_content(); ?>
                       <div class="postmetadata tags">
 					  		<?php the_tags(); ?>
