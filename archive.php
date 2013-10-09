696a01fa2ccc40be930cdb5ea04752fd34986331
RYO NAGAI <seinaoasis@gmail.com>
2013-10-09 18:30:33 +0900

    アーカイブでのタイトルテキストの削除

--- archive.php
+++ archive.php
@@ -30,7 +30,6 @@
                         <div><a href="<?php the_permalink() ?>" class="preview"><?php echo "$thumbID"; ?></a></div>
                     	<?php } ?>
 
-                    <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
                                         <?php the_tags(''); ?>
                     <div class="postmetadata">
                         <?php the_time(__('Y年m月d日(D)')) ?>&nbsp;&nbsp;
